aaaa96fd06a0d12dec74a90e7f1d616469f93c45
BelierJavier <Zathoroth@icloud.com>
2024-03-13 01:55:59 -0400

    Signup form complete

--- Views/SignupView.swift
+++ Views/SignupView.swift
@@ -12,7 +12,6 @@ struct SignupView: View {
     @State private var email = ""
     @State private var pass = ""
     @State private var repass = ""
-
     
     var body: some View {
         NavigationView {
@@ -44,6 +43,9 @@ struct SignupView: View {
                         .foregroundStyle(.white)
                         .background(Color(.black))
                         .cornerRadius(15)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(.red, lineWidth: !email.isEmpty && !isEmail() ? 3 : 0))
                         .padding([.horizontal], 45)
                         .padding(.bottom, 15)
                     
@@ -55,6 +57,9 @@ struct SignupView: View {
                         .foregroundStyle(.white)
                         .background(Color(.black))
                         .cornerRadius(15)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(.yellow, lineWidth: !securePass() && !pass.isEmpty ? 3 : 0))
                         .padding([.horizontal], 45)
                         .padding(.bottom, 15)
                     
@@ -66,6 +71,9 @@ struct SignupView: View {
                         .foregroundStyle(.white)
                         .background(Color(.black))
                         .cornerRadius(15)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(.red, lineWidth: !passConfirm() && !repass.isEmpty ? 3 : 0))
                         .padding([.horizontal], 45)
                         .padding(.bottom, 30)
                     
@@ -79,15 +87,15 @@ struct SignupView: View {
                                 .fontWeight(.bold)
                                 .padding()
                                 .frame(maxWidth: .infinity)
-                                .foregroundStyle(.white)
-                                .background(Color(.red))
-                                .cornerRadius(15)
                         }
+                        .disabled((email.isEmpty || pass.isEmpty || repass.isEmpty) || (!isEmail()) || (!passConfirm()) || (!securePass()))
+                        .buttonStyle(RegButtonStyle())
                         
                     }
-                    .shadow(color: Color("redglow"), radius: 10, x: 0, y: 5)
                     .padding([.horizontal], 45)
                     .padding(.bottom, 10)
+                    
+                    
             
                 }
                 .foregroundColor(.white)
@@ -96,11 +104,47 @@ struct SignupView: View {
     }
     
     func Register() {
-        Auth.auth().createUser(withEmail: email, password: pass) {result, error in
-            if error != nil {
-                print(error!.localizedDescription)
+        if (!email.isEmpty || !pass.isEmpty || !repass.isEmpty) {
+            if ((passConfirm()) && isEmail()) {
+                Auth.auth().createUser(withEmail: email, password: pass) {result, error in
+                    if error != nil {
+                        print(error!.localizedDescription)
+                    }
+                }
+            }
+            else {
+                
             }
         }
+    }
+    
+    func isEmail() -> Bool{
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
+    
+    func securePass() -> Bool{
+        let passRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[d$@$!%*?&#])[A-Za-z\\dd$@$!%*?&#]{8,}"
+        return pass.range(of: passRegex, options: .regularExpression, range: nil, locale: nil) != nil
+    }
+    
+    func passConfirm() -> Bool{
+        return pass == repass
+    }
+}
+
+struct RegButtonStyle: ButtonStyle {
+    @Environment(\.isEnabled) var isEnabled
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .background(!isEnabled ? Color.gray : (configuration.isPressed ? Color.white : Color.red))
+            .foregroundStyle(configuration.isPressed ? Color.black : Color.white)
+            .cornerRadius(15)
+            .shadow(color:!isEnabled ? .red.opacity(0.0) : Color("redglow").opacity(1.0), radius: 10, x: 0, y: 5)
+            .animation(.easeInOut(duration: 0.2), value: isEnabled)
+            
     }
 }
 
