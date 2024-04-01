2aacca2db6d14d7eb30760ea2e17581739e02f41
BelierJavier <Zathoroth@icloud.com>
2024-04-01 18:47:50 -0400

    Editing Friends Tab

--- App Content/FriendsView.swift
+++ App Content/FriendsView.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 struct FriendsView: View {
+    @State private var username = ""
     var body: some View {
         ZStack {
+            Color("spacegray")
+                .ignoresSafeArea()
             VStack {
+                TextField("Search", text: $username, prompt: Text("Find new friends").foregroundStyle(Color(.gray)))
+                    .font(.subheadline)
+                    .fontWeight(.regular)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(.white)
+                    .background(Color(.black))
+                    .cornerRadius(15)
+                    .padding([.horizontal], 45)
+                    .padding(.bottom, 15)
                 Text("Friends")
                     .bold()
                     .foregroundStyle(Color(.white))
+                
             }
         }
     }
